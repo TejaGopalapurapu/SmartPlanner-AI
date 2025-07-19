2a2c0bcf5d8873140c024933b8d3d57554aff09c
TejaGopalapurapu <gopalapuraputeja@gmail.com>
2025-07-20 00:41:42 +0530

    Initial fullstack AI planner commit

---
+++ Server/Server/Controllers/TaskController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Server.Models;
+
+namespace Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TaskController : ControllerBase
+    {
+        private static List<TaskItem> tasks = new();
+        private static int nextId = 1;
+
+        [HttpPost]
+        public IActionResult AddTask([FromBody] TaskItem task)
+        {
+            task.Id = nextId++;
+            tasks.Add(task);
+            return Ok(tasks);
+        }
+
+        [HttpGet]
+        public IActionResult GetTasks()
+        {
+            return Ok(tasks);
+        }
+    }
+}
---
+++ Server/Server/Models/TaskItem.cs
@@ -0,0 +1,8 @@
+﻿namespace Server.Models
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+    }
+}
